ce3e434e1eb1401f374b2be1fcca05cbd128b87d
ArtDiaNic <Artdianic@gmail.com>
2022-02-14 22:14:43 +0300

    added pom-pages

--- src/test/java/tests/PriceAndNameOfProductTest.java
+++ src/test/java/tests/PriceAndNameOfProductTest.java
@@ -1,36 +1,41 @@
 package tests;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.RegistrationPOMPage;
+import pages.ShoppingCartPOMPage;
 import staticdata.WebUrl;
 
 public class PriceAndNameOfProductTest extends BaseTest {
-
-
+    RegistrationPOMPage registrationPOMPage;
+    ShoppingCartPOMPage shoppingCartPOMPage;
     @Test
     public void priceTest() {
-        driver.get(WebUrl.SAUCE_DEMO_URL);
-        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys(WebUrl.EMAIL);
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys(WebUrl.PASSWORD);
-        driver.findElement(By.xpath("//input[@value='Login']")).click();
-        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
-        String priceOnPage = driver.findElement(By.xpath("//div[@class='inventory_item_price']")).getText();
-        driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
-        String priceInShoppingCart = driver.findElement(By.xpath("//div[@class='inventory_item_price']")).getText();
+        registrationPOMPage = new RegistrationPOMPage(driver);
+        shoppingCartPOMPage = new ShoppingCartPOMPage(driver);
+        registrationPOMPage.openRegistrationPage();
+        registrationPOMPage.inputIntoEmail(WebUrl.EMAIL);
+        registrationPOMPage.inputIntoPassword(WebUrl.PASSWORD);
+        registrationPOMPage.registrationButton();
+        shoppingCartPOMPage.addProductToShopCart();
+        String priceOnPage = shoppingCartPOMPage.priceOnPage();
+        shoppingCartPOMPage.goToShoppingCart();
+        String priceInShoppingCart = shoppingCartPOMPage.priceInShoppingCart();
         Assert.assertEquals(priceInShoppingCart, priceOnPage, "Prices are not equal");
     }
 
     @Test
     public void nameTest() {
-        driver.get(WebUrl.SAUCE_DEMO_URL);
-        driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys(WebUrl.EMAIL);
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys(WebUrl.PASSWORD);
-        driver.findElement(By.xpath("//input[@value='Login']")).click();
-        driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-backpack']")).click();
-        String nameOnPage = driver.findElement(By.xpath("//a[@id='item_4_title_link']/div[@class='inventory_item_name']")).getText();
-        driver.findElement(By.xpath("//a[@class='shopping_cart_link']")).click();
-        String nameInShoppingCart = driver.findElement(By.xpath("//div[@class='inventory_item_name']")).getText();
+        registrationPOMPage = new RegistrationPOMPage(driver);
+        shoppingCartPOMPage = new ShoppingCartPOMPage(driver);
+        registrationPOMPage.openRegistrationPage();
+        registrationPOMPage.inputIntoEmail(WebUrl.EMAIL);
+        registrationPOMPage.inputIntoPassword(WebUrl.PASSWORD);
+        registrationPOMPage.registrationButton();
+        shoppingCartPOMPage.addProductToShopCart();
+        String nameOnPage = shoppingCartPOMPage.nameOnPage();
+        shoppingCartPOMPage.goToShoppingCart();
+        String nameInShoppingCart = shoppingCartPOMPage.nameInShoppingCart();
         Assert.assertEquals(nameInShoppingCart, nameOnPage, "Names are not equal");
     }
 }
