cc03797adb3c73f8918cccbd297462f8bce9d623
ArtDiaNic <Artdianic@gmail.com>
2022-03-10 15:40:46 +0300

    Screenshots was added

--- src/test/java/driver/factorydriver/RemoteDriverManager.java
+++ src/test/java/driver/factorydriver/RemoteDriverManager.java
@@ -17,6 +17,6 @@ public class RemoteDriverManager extends DriverManager {
         browserOptions.setCapability("browserVersion", "99");
         Map<String, Object> sauceOptions = new HashMap<>();
         browserOptions.setCapability("sauce:options", sauceOptions);
-        driver = new RemoteWebDriver(new URL("https://oauth-nddpmailbox-9e0b1:af1ac9c7-3c51-4833-959a-a5aeb87be1d4@ondemand.eu-central-1.saucelabs.com:443/wd/hub"), browserOptions);
+        driver = new RemoteWebDriver(new URL("https://oauth-artdianic-8d06b:11d3ad12-2491-4b08-82ab-d2efbfb90a84@ondemand.eu-central-1.saucelabs.com:443/wd/hub"), browserOptions);
     }
 }
